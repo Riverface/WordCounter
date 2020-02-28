bd19d0ee07ea99e4bde55ae930bf54e1f700378c
Akigawa <sonoskeakigawa@gmail.com>
2020-02-28 12:51:08 -0800

    Complete refactor of tests and specs.

--- WordCounter.Tests/Models/WordCounter.Tests.cs
+++ WordCounter.Tests/Models/WordCounter.Tests.cs
@@ -7,21 +7,43 @@ namespace WordCounter.Tests
     public class WordCounterTests
     {
         [TestMethod]
-        public void Word_Counts_Word()
+        public void Count_AreWordsIdenticalMatches_True()
         {
             Counter testcounter = new Counter("yeah", "yeah");
-            testcounter.Dissect();
-            testcounter.Count("word-in-word");
+            testcounter.Count();
             Assert.AreEqual(1, testcounter.Amount);
         }
         [TestMethod]
-        public void Word_Counts_Sentence()
+        public void RunWordCounter_IgnoresCapitals_True()
         {
-            Counter testcounter = new Counter("yeah", "yeah, I like to dance, yeah");
-            testcounter.Dissect();
-            testcounter.Count("word-in-sentence");
-            Assert.AreEqual(2, testcounter.Amount);
+            Counter testcounter = new Counter("cat", "Cat");
+            testcounter.RunWordCounter();
+            Assert.AreEqual(1, testcounter.Amount);
         }
+
+        [TestMethod]
+        public void RunWordCounter_IgnoresNumbers_True()
+        {
+            Counter testcounter = new Counter("cat", "1Cat");
+            testcounter.RunWordCounter();
+            Assert.AreEqual(1, testcounter.Amount);
+        }
+        [TestMethod]
+        public void RunWordCounter_IgnoresPunctuation_True()
+        {
+            Counter testcounter = new Counter("cat", "!1Cat");
+            testcounter.RunWordCounter();
+            Assert.AreEqual(1, testcounter.Amount);
+        }
+
+        [TestMethod]
+        public void RunWordCounter_NoPartialMatches_True()
+        {
+            Counter testcounter = new Counter("cat", "!1Cathedral");
+            testcounter.RunWordCounter();
+            Assert.AreEqual(0, testcounter.Amount);
+        }
+
 
     }
 }
